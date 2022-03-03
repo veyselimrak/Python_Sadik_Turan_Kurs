eb7596bc273745208f003f00fec14b0f1b3c8532
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-03-03 14:54:26 +0300

    Add files via upload

---
+++ applications/fourth_app.py
@@ -0,0 +1,8 @@
+yariCap = input("Yarı Çapı Giriniz : ");
+yariCap = float(yariCap);
+pi = 3.14;
+darireninAlani = pi * yariCap**2;
+dairenincevresi = 2 * pi * yariCap;
+
+print("Dairenin Çevresi : ",dairenincevresi);
+print("Dairenin Alanı : " ,darireninAlani);
