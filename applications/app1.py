834364f25b122b16a05eecf7fdc0fdcd9cf84ae6
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-04-10 16:29:14 +0300

    Add files via upload

---
+++ applications/app1.py
@@ -0,0 +1,1 @@
+print("Baba Geri Geldi...")
---
+++ applications/app13.py
@@ -0,0 +1,9 @@
+x = input("1.Sayıyı Giriniz : ");
+y = input("2.Sayıyı Giriniz : py p");
+
+print(type(x));
+print(type(y));
+
+toplam = int(x) + int(y);
+
+print(toplam);
