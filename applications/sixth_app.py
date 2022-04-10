098e70e85ebd2a1573d228f0540bbf47023e218b
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-04-10 16:27:29 +0300

    Delete sixth_app.py

--- applications/sixth_app.py
+++
@@ -1,9 +0,0 @@
-name = "Veysel"
-surname = "İmrak"
-age = 22
-
-print("Adı : {} \nSoyadı : {} \nYaşı : {}".format(name,surname,age))
-kesir = 400/700
-print( "Kesirli Sayı : {:1.3} ".format(kesir))
-print(f"Adı : {name} \nSoyadı : {surname} \nYaşı : {age}  ")
-print( f"Kesirli Sayı : {kesir:1.2} ")
