b4306680a2597ae28014cceb291c37592b25b03a
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-04-10 16:26:38 +0300

    Delete fourth_app.py

--- applications/fourth_app.py
+++
@@ -1,8 +0,0 @@
-yariCap = input("Yarı Çapı Giriniz : ");
-yariCap = float(yariCap);
-pi = 3.14;
-darireninAlani = pi * yariCap**2;
-dairenincevresi = 2 * pi * yariCap;
-
-print("Dairenin Çevresi : ",dairenincevresi);
-print("Dairenin Alanı : " ,darireninAlani);
