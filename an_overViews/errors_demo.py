79806c76463bb5de8b4c48600d714a2b5d6b9963
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-05-12 14:31:17 +0300

    Update errors_demo.py

--- an_overViews/errors_demo.py
+++ an_overViews/errors_demo.py
@@ -1,43 +1,43 @@
 
-# liste = ["1","2","5a","10b","abc","10","50"]
+liste = ["1","2","5a","10b","abc","10","50"]
 
-# for i in liste:
-#     try:
-#         int(i)
-#     except Exception as ex:
-#         print(f"listenin {i} elemanı int değerine dönüştürülemiyor.\Hata türü: {ex}")
-#     else:
-#         print(f"listenin {i} elemanı int değerine dönüştürüldü.")
+for i in liste:
+    try:
+        int(i)
+    except Exception as ex:
+        print(f"listenin {i} elemanı int değerine dönüştürülemiyor.\Hata türü: {ex}")
+    else:
+        print(f"listenin {i} elemanı int değerine dönüştürüldü.")
 
 import re
 
-# while True:
-#     sayi = input("Sayı değeri giriniz: ")
-#     if sayi == "q":
-#         break
-#     else:
-#         try:
-#             float(sayi)
-#         except Exception as hata:
-#             print("Sayı değeri girmediniz.\nHata türü: {}".format(hata))
-#         else:
-#             print("Geçerli giriş")
-#             break
+while True:
+    sayi = input("Sayı değeri giriniz: ")
+    if sayi == "q":
+        break
+    else:
+        try:
+            float(sayi)
+        except Exception as hata:
+            print("Sayı değeri girmediniz.\nHata türü: {}".format(hata))
+        else:
+            print("Geçerli giriş")
+            break
 
 
-# def checkPassword(password):
-#     if re.search("[çüöıÇÖÜİ1]",password):
-#         raise Exception()
+def checkPassword(password):
+    if re.search("[çüöıÇÖÜİ1]",password):
+        raise Exception()
 
 
-# parola = input("Parola Giriniz: ")
+parola = input("Parola Giriniz: ")
 
-# try:
-#     checkPassword(parola)
-# except Exception as ex:
-#     print("Şifre Türkçe karakter içermemelidir.")
-# else:
-#     print("geçerli şifre")
+try:
+    checkPassword(parola)
+except Exception as ex:
+    print("Şifre Türkçe karakter içermemelidir.")
+else:
+    print("geçerli şifre")
 
 def factorial(x):
     if x > 0:
