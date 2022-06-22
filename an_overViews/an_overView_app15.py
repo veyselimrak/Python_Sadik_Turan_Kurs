6d979fb2560fdc5aef0500baff992c5f87f1fb9c
Veysel <78259944+Veysel104@users.noreply.github.com>
2022-06-23 01:07:17 +0300

    Update an_overView_app15.py

--- an_overViews/an_overView_app15.py
+++ an_overViews/an_overView_app15.py
@@ -1,54 +1,54 @@
-#def toplama(x, y):
-#     z = x + y 
-#     print(z)
+def toplama(x, y):
+    z = x + y 
+    print(z)
 
 
-# toplama(5,6)
-
-
-
-
-# def tekrarlama(kelime, kacTekrar):
-#     print(kelime * kacTekrar)
-# tekrarlama("Veysel\n",2)
+toplama(5,6)
 
 
 
-# def sinirsizList(*parametreler):
-#     list = []
 
-#     for parametre in parametreler:
-#         list.append(parametre)
-
-#     return list
-# result = sinirsizList(10,20,30,40,50,"Veysel")
-
-# print(result)
-
-# def asalSayilariBulma(sayi1, sayi2):
-#     for sayi in range(sayi, sayi2+1):
-#         if sayi > 1:
-#             for i in range(2,sayi):
-#                 if (sayi % i == 0):
-#                     break
-#             else:
-#                     print(sayi)
-
-# sayi1 = int(input("Küçük Sayıyı Giriniz :"))
-# sayi2 = int(input("Büyük Sayıyı Giriniz : "))
-
-# asalSayilariBulma(sayi1, sayi2)
+def tekrarlama(kelime, kacTekrar):
+    print(kelime * kacTekrar)
+tekrarlama("Veysel\n",2)
 
 
 
-# def tamBolenleriBul(sayi):
+def sinirsizList(*parametreler):
+    list = []
 
-#     tamBolenler = []
+    for parametre in parametreler:
+        list.append(parametre)
 
-#     for i in range(2, sayi):
-#         if (sayi % i == 0):
-#             tamBolenler.append(i)
+    return list
+result = sinirsizList(10,20,30,40,50,"Veysel")
+
+print(result)
+
+def asalSayilariBulma(sayi1, sayi2):
+    for sayi in range(sayi, sayi2+1):
+        if sayi > 1:
+            for i in range(2,sayi):
+                if (sayi % i == 0):
+                    break
+            else:
+                    print(sayi)
+
+sayi1 = int(input("Küçük Sayıyı Giriniz :"))
+sayi2 = int(input("Büyük Sayıyı Giriniz : "))
+
+asalSayilariBulma(sayi1, sayi2)
+
+
+
+def tamBolenleriBul(sayi):
+
+    tamBolenler = []
+
+    for i in range(2, sayi):
+        if (sayi % i == 0):
+            tamBolenler.append(i)
     
-#     return tamBolenler
+    return tamBolenler
 
-# print(tamBolenleriBul(40))
+print(tamBolenleriBul(40))
